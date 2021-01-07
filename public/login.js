5045b629e73aaa3b63e1d42ede76250084e9e6c2
Amol Samota <samota.amol.21apr@gmail.com>
2021-01-07 11:59:18 +0530

    google auth added

--- public/login.js
+++ public/login.js
@@ -3,7 +3,7 @@ var uiConfig = {
     signInSuccessUrl: 'index.html',
     signInOptions: [
       // Leave the lines as is for the providers you want to offer your users.
-            //firebase.auth.GoogleAuthProvider.PROVIDER_ID,
+            firebase.auth.GoogleAuthProvider.PROVIDER_ID,
             //firebase.auth.FacebookAuthProvider.PROVIDER_ID,
             //firebase.auth.TwitterAuthProvider.PROVIDER_ID,
             //firebase.auth.GithubAuthProvider.PROVIDER_ID,
